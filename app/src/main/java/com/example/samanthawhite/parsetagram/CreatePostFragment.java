b77a1811189170b13e2d65d1ad00bf73b6405fb1
Samantha White <samanthawhite@fb.com>
2018-07-11 16:15:53 -0700

    issue with createpostfragments

--- app/src/main/java/com/example/samanthawhite/parsetagram/CreatePostFragment.java
+++ app/src/main/java/com/example/samanthawhite/parsetagram/CreatePostFragment.java
@@ -1,24 +1,197 @@
 package com.example.samanthawhite.parsetagram;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.samanthawhite.parsetagram.model.Post;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+import static android.app.Activity.RESULT_OK;
+import static com.parse.ManifestInfo.getPackageManager;
 
 public class CreatePostFragment extends Fragment {
 
+    public final String APP_TAG = "MyCustomApp";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
+    File photoFile;
+    public final String TAG = "HomeActivity";
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    }
+    @BindView(R.id.etDescription) EditText descriptionInput;
+    @BindView(R.id.btnCreate) Button createButton;
+    @BindView(R.id.takepic_btn) Button pictureButton;
+    @BindView(R.id.picture) ImageView ivPreview;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_post, container, false);
+
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //put basically all the code here
+
+        //ButterKnife.bind(this, view);
+
+
+    }
+
+    @OnClick(R.id.btnCreate)
+    protected void createButtonClick(){
+        final String description = descriptionInput.getText().toString();
+        final ParseUser user = ParseUser.getCurrentUser();
+
+        // IS THIS SUPPOSED TO RETURN A FILE OR A URI??
+        File photoFile = getPhotoFile(photoFileName);
+
+        // IT SEEMS LIKE PARSE FILES HAS TO TAKE IN BYTE?
+        // https://docs.parseplatform.org/android/guide/#files
+        ParseFile parseFile = new ParseFile(photoFile);
+
+        createPost(description, parseFile, user);
+    }
+
+
+    @OnClick(R.id.takepic_btn)
+    protected void takePicture(){
+        onLaunchCamera(view);
+    }
+
+
+    public void onLaunchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFile(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See
+        // https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(this.getContext(), "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will
+        // crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFile(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+
+                takenImage = BitmapScaler.scaleToFitWidth(takenImage, 100);
+                // TODO RESIZE BITMAP, see section below but first understand how URI is used
+                // Uri takenPhotoUri = getPhotoFileUri(photoFileName);
+
+                // Configure byte output stream
+                try {
+                    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                    // Compress the image further
+                    takenImage.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+                    // Create a new file for the resized bitmap (`getPhotoFileUri` defined above)
+                    File resizedFile = new File(photoFile.getAbsolutePath());
+                    resizedFile.createNewFile();
+                    FileOutputStream fos = null;
+                    fos = new FileOutputStream(resizedFile);
+                    fos.write(bytes.toByteArray());
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // Write the bytes of the bitmap to file
+
+                // Bitmap bMapScaled = Bitmap.createScaledBitmap(bMap, 150, 100, true);
+
+                // Load the taken image into a preview
+                ivPreview.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    private void createPost(String description, ParseFile imageFile, ParseUser user) {
+        final Post newPost = new Post();
+        newPost.setDescription(description);
+        newPost.setImage(imageFile);
+        newPost.setUser(user);
+
+        newPost.saveInBackground(
+                new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Log.d("HomeActivity", "create post success");
+
+                        } else {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+    }
+
+
 }
